4164b321e6a7cb304e09863017d789d2d2169edb
Irfan mba <ip.5090@gmail.com>
2024-11-20 10:32:16 +0800

    login page tweakˆ

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -49,7 +49,7 @@
             <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
                 <div class="bg-light rounded p-4 p-sm-5 my-4 mx-3">
                     <div class="d-flex align-items-center justify-content-between mb-3">
-                        <a href="index.html" class="">
+                        <a href="#" class="">
                             <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>SIMTD</h3>
                         </a>
                         <h3>Sign In</h3>
@@ -77,12 +77,15 @@
                             </span>
                             @enderror
                         </div>
+                        <button type="submit" class="btn btn-primary py-3 w-100 mb-2">Sign In</button>
                         @if (Route::has('password.request'))
-                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                            <a class="btn btn-secondary py-3 w-100 mb-2" href="{{ route('password.request') }}">
                                 {{ __('Forgot Your Password?') }}
                             </a>
                         @endif
-                        <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Sign In</button>
+                        @if (Route::has('register'))
+                            <a class="btn btn-secondary py-3 w-100 mb-2" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        @endif
                     </form>
                 </div>
             </div>
